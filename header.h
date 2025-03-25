2f7c90b61b82ac2533d414df0b88c7bc8178ce84
cty0613 <icty0613@gmail.com>
2025-03-25 11:55:12 +0900

    refactor dir structure

---
+++ header.h
@@ -0,0 +1,9 @@
+//
+//  header.h
+//  vede_cpplecture
+//
+//  Created by 최태영 on 3/25/25.
+//
+
+
+int sum(int a, int b);
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+
+    string name = argv[1];
+    string age = argv[2];
+    
+
+    // cout << "input your name" << endl;
+    // cin >> name;
+    // cout << "input your age" << endl;
+    // cin >> age;
+   
+    cout << "Your Name is " << name << " and Your Age is " << age << endl;
+    return 0;
+}
