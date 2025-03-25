d517d04d9856ad2024ceaf8ce700dad727fad0e5
cty0613 <icty0613@gmail.com>
2025-03-25 10:19:29 +0900

    Initial Commit

---
+++ vede_cpplecture/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  vede_cpplecture
+//
+//  Created by 최태영 on 3/25/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
