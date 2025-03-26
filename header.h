35e16b8780f069c95cce3acd022d0bbd1822e924
cty0613 <icty0613@gmail.com>
2025-03-26 16:56:11 +0900

    lecture 0326_1

--- header.h
+++
@@ -1,9 +0,0 @@
-//
-//  header.h
-//  vede_cpplecture
-//
-//  Created by 최태영 on 3/25/25.
-//
-
-
-int sum(int a, int b);
--- ref_swap.cpp
+++ ref_swap.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 
-void swap(int x, int y); // 적절하지 않은 함수
+// void swap(int x, int y); // 적절하지 않은 함수
 void swap_ptr(int* x, int* y); // 포인터를 이용한 함수
-void swap_ref(int& x, int& y); 
-// 참조자를 이용한 함수 => 원본의 별명을 전달받음 => 변수 원본에 접근하여 수정!
+void swap_ref(int& x, int& y); // 원본의 별명(레퍼런스)을 전달받음 => 원본에 접근하여 수정!
 
 int main() {
 
+    int a = 10;
+    int b = 20;
+
+    std::cout << "Before swap" << std::endl;
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+
+    swap_ref(a, b);
+//  swap_ptr(&a, &b);
+
+    std::cout << std::endl;
+    std::cout << "After swap" << std::endl;
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
     return 0;
+}
+
+void swap_ref(int &x, int &y){
+    int t = x;
+    x = y;
+    y = t;
+}
+
+void swap_ptr(int *x, int *y) {
+    int t = *x;
+    *x = *y;
+    *y = t;
 }
