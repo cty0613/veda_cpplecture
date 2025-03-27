e797a084c857d8b4b61ba82907ad434c0d9eff61
cty0613 <icty0613@gmail.com>
2025-03-27 16:30:00 +0900

    lecture 0327_2

--- class_excercise2.cpp
+++ class_excercise2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,9 +8,10 @@ class Car{
         int fuel;
         int drive_cost;
         int meter;
+        string name;
 
     public: 
-        Car(int _fuel, int _drive_cost);
+        Car(string _name, int _fuel, int _drive_cost);
         ~Car();
         int drive(int accel);
         int refuel(int refuel_amount);
@@ -18,13 +20,15 @@ class Car{
       
 };
 
-Car::Car(int _fuel, int _drive_cost){
+Car::Car(string _name, int _fuel, int _drive_cost){
     fuel = _fuel;
     drive_cost = _drive_cost;
+    name = _name;
     meter = 0;
-    cout << "object Car has created." << endl;
+    cout << "Car ["<< _name << "]" <<  " is created." << endl;
     cout << "    >>  init fuel : " << fuel << endl;
     cout << "    >>  drive cost : " << drive_cost << endl;
+    cout << endl;
 }
 
 Car::~Car(){
@@ -33,13 +37,17 @@ Car::~Car(){
 
 int Car::drive(int accel) {
     fuel -= accel*drive_cost;
-    meter = meter + (accel*5);
-
-    return accel*5;
+    int ahead = accel*5;
+    meter = meter + ahead;
+    cout << "drive " << ahead << "km" << endl;
+    cout << "meter " << getMeter() << "km" << endl;
+    return ahead;
 }
 
 int Car::refuel(int refuel_amount) {
     fuel = fuel + refuel_amount;
+    cout << "refuel " << refuel_amount << "L" << endl;
+    cout << "fuel remained : " << getFuel() << endl;
     return fuel;
 }
 
@@ -51,26 +59,50 @@ int Car::getMeter() const {
     return meter;
 }
 
-
-
-
 int main(){
-    Car* myCar = new Car(100, 5);
+    string car_name;
+    int init_fuel;
+    int init_di; 
 
-    cout << endl;
-    cout << "myCar Moved : " << myCar->drive(2) << "km" << endl;
-    cout << "myCar's remained fuel : " << myCar->getFuel() << "L" << endl;
+    cout << "Create New Car ..." << endl;
+    cout << "Car Name? : ";
+    cin >> car_name;
+
+    cout << "initial Fuel? : ";
+    cin >> init_fuel;
     
-    int refuel;
-    cout << "input refuel amount : ";
-    cin >> refuel;
+    cout << "Drive Cost? : ";
+    cin >> init_di;
 
-    myCar->refuel(refuel);
-    cout << "myCar refueled... " << endl;
-    cout << "myCar's remained fuel : " << myCar->getFuel() << "L" << endl;
+    Car* myCar = new Car(car_name, init_fuel, init_di);
 
+    while(1) {
+        int operation;
+    
+        cout << "1 : drive / 2 : refuel / 3 : view meter / 4 : exit" << endl;
+        cout << "-------------------------" << endl;
+        cout << "Select Operation : ";
+        cin >> operation;
 
-    cout << "myCar's meter : " << myCar->getMeter() << "km" << endl;
+        if (operation == 1) {
+            int user_accel;
+            cout << "Accel Amount? : ";
+            cin >> user_accel;
+            myCar->drive(user_accel);
+            
+        } else if (operation == 2) {
+            int user_refuel;
+            cout << "Refuel Amount? : ";
+            cin >> user_refuel;
+            myCar->refuel(user_refuel);
+
+        } else if (operation == 3) {
+            cout << "meter : " << myCar->getMeter() << endl;
+        } else if (operation == 4){
+            break;
+        }
+    }
+    
 
     delete myCar;
 
